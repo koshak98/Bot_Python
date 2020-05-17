14755442e7bd632a8ea4c579d8c79e81ead3b216
koshak98 <kleshev.maksim@bk.ru>
2020-05-17 15:22:01 +0300

    del

--- SportBot/Bot_Python/README.md
+++
@@ -1,2 +0,0 @@
-# Bot_Python
-This bot is made on python. It helps to find info about your favorite football club.
--- SportBot/select_users.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-with sqlite3.connect("clubs.db") as conn:
-    cur = conn.cursor()
-    print(cur.execute('SELECT * FROM user;').fetchall())
-
